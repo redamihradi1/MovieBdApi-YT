0b9933365b0e4e11cfaf56e9b256125ec2c10069
Reda Mihradi <mihradi_reda@emsi-edu.ma>
2022-03-30 22:19:22 +0100

    Realisation d'un app en reacte native qui permet de voir si un film existe dans ma base de donnée fourni par une api

--- App.js
+++ App.js
@@ -1,11 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  TouchableHighlight,
+  Modal,
+} from "react-native";
+import axios from "axios";
+import React, { useState } from "react";
 
 export default function App() {
+  const apiurl = "http://www.omdbapi.com/?i=tt3896198&apikey=c8c642cd";
+  const [state, setState] = useState({
+    s: "Entrer le nom du film",
+    results: [],
+    selected: {},
+  });
+  const search = () => {
+    axios(apiurl + "&s=" + state.s).then(({ data }) => {
+      let results = data.Search;
+      setState((prevState) => {
+        return { ...prevState, results: results };
+      });
+    });
+  };
+
+  const openPopup = (id) => {
+    axios(apiurl + "&i=" + id).then(({ data }) => {
+      let result = data;
+      console.log(result);
+      setState((prevState) => {
+        return { ...prevState, selected: result };
+      });
+    });
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Film DB </Text>
+      <TextInput
+        style={styles.searchbox}
+        onChangeText={(text) =>
+          setState((prevState) => {
+            return { ...prevState, s: text };
+          })
+        }
+        onSubmitEditing={search}
+        value={state.s}
+      />
+
+      <ScrollView style={styles.results}>
+        {state.results.map((result) => (
+          <TouchableHighlight
+            key={result.IMDbID}
+            onPress={() => openPopup(result.IMDbID)}
+          >
+            <View key={result.IMDbID} style={styles.result}>
+              <Text style={styles.heading}>{result.Title}</Text>
+            </View>
+          </TouchableHighlight>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +69,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#223343",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: 70,
+    paddingHorizontal: 20,
+  },
+  title: {
+    color: "#FFF",
+    fontSize: 32,
+    fontWeight: "700",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  searchbox: {
+    fontSize: 20,
+    fontWeight: "300",
+    padding: 20,
+    width: "40%",
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    marginBottom: 40,
+  },
+  results: {
+    flex: 1,
+  },
+  result: {
+    flex: 1,
+    width: "100%",
+    marginBottom: 20,
+  },
+  heading: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "40%",
+    padding: 20,
+    backgroundColor: "#445565",
   },
 });
